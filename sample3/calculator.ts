f8b064752f36732f01be1ce772d74dff72ac2384
JGo <jayson.go@omf.com>
2025-11-19 03:09:34 -0700

    comments

--- sample3/calculator.ts
+++ sample3/calculator.ts
@@ -6,19 +6,21 @@ class PersonalCalculator {
         const client = new AuthClient();
         await client.login(); // This take 100ms
 
-        // Aha!
+        // Coding log Day 23: Me again. Eureka! I figured it out!!!
         // Bug fix for jira.com/issues/12345
         // Determined there is a race condition between the client.login and the availability of the token
         // Adding this amazing function to solve the race condition
+        // ~Mr. Tax
         await this.waitUntilInitialized(client.token?.attributes.userId);
 
         const greeter = new PersonalizationClient();
         await greeter.init(client.token?.attributes.userId);
 
+        // Coding log Day 4: Initial assessment of some reported problem
         // While working on jira.com/issues/12345, I noticed this would always output "Argh! Ahoy!, John"
         // Based on requirements, this should either be Hey, John or Hi, John
         // There's a race condition but I'm confused...firstName is availalble but somehow greeting is broken (because userId was null)
-        // Mr. Syn Tax
+        // This is Syn
         console.log(`${greeter.greeting}, ${client.token?.attributes.firstName}`);
     }
 
